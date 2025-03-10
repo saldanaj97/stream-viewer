a7624ee47b07ecc0ecb248592fa92d1aa1083e68
saldanaj97 <saldanaj97@gmail.com>
2025-03-10 16:26:22 -0500

    feat: add Zustand state management for sidebar and user

---
+++ stores/sidebar-store.ts
@@ -0,0 +1,25 @@
+import { createStore } from "zustand";
+
+export type SidebarState = {
+  isSidebarOpen: boolean;
+};
+
+export type SidebarActions = {
+  toggleSidebar: () => void;
+};
+
+export type SidebarStore = SidebarState & SidebarActions;
+
+export const createSidebarStore = () => {
+  return createStore<SidebarStore>((set) => ({
+    isSidebarOpen: false,
+    toggleSidebar: () =>
+      set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
+  }));
+};
+
+// const useSidebarStore = create<SidebarStore>((set) => ({
+//   isSidebarOpen: false,
+//   toggleSidebar: () =>
+//     set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
+// }));
---
+++ stores/user-store.ts
@@ -0,0 +1,29 @@
+import { createStore } from "zustand";
+
+export type User = {
+  id?: string;
+  name?: string;
+};
+
+export type UserState = {
+  user: User | null;
+};
+
+export type UserActions = {
+  setUser: (user: User) => void;
+};
+
+export type UserStore = UserState & UserActions;
+
+export const createUserStore = () => {
+  return createStore<UserStore>((set) => ({
+    user: null,
+    setUser: (user: User) => set({ user }),
+    clearUser: () => set({ user: null }),
+  }));
+};
+
+// const useUserStore = create<UserStore>((set) => ({
+//   user: null,
+//   setUser: (user: User) => set({ user }),
+// }));
