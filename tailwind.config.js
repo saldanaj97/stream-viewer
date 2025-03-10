9eb036a76572fb45d5e0e2337610327eb1aff89b
saldanaj97 <saldanaj97@gmail.com>
2025-03-09 21:54:06 -0500

    update tailwind configuration to include features and styles directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,11 @@ import { heroui } from "@heroui/theme";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/components/(button|code|input|kbd|link|listbox|modal|navbar|snippet|toggle|ripple|spinner|form|divider|popover).js",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.css",
   ],
   theme: {
     extend: {
