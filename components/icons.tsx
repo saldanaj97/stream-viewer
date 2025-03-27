8e7fe406db422c776e6bb71a24900892a30a0f0c
saldanaj97 <saldanaj97@gmail.com>
2025-03-27 12:50:20 -0500

    feat: enhance icon components with size props and add Check and X icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -183,44 +183,102 @@ export const SearchIcon = (props: IconSvgProps) => (
   </svg>
 );
 
-export function TwitchIcon() {
+export function TwitchIcon({
+  size = 16,
+  width,
+  height,
+  ...props
+}: IconSvgProps) {
   return (
     <svg
       fill="currentColor"
-      height="16"
+      height={size || height}
       viewBox="0 0 24 24"
-      width="16"
+      width={size || width}
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path d="M11.571 4.714h1.715v5.143H11.57zm4.715 0H18v5.143h-1.714zM6 0L1.714 4.286v15.428h5.143V24l4.286-4.286h3.428L22.286 12V0zm14.571 11.143l-3.428 3.428h-3.429l-3 3v-3H6.857V1.714h13.714z" />
     </svg>
   );
 }
 
-export function YouTubeIcon() {
+export function YouTubeIcon({
+  size = 16,
+  width,
+  height,
+  ...props
+}: IconSvgProps) {
   return (
     <svg
       fill="currentColor"
-      height="16"
+      height={size || height}
       viewBox="0 0 24 24"
-      width="16"
+      width={size || width}
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
     </svg>
   );
 }
 
-export function KickIcon() {
+export function KickIcon({ size = 16, width, height, ...props }: IconSvgProps) {
   return (
     <svg
       fill="currentColor"
-      height="16"
+      height={size || height}
       viewBox="0 0 24 24"
-      width="16"
+      width={size || width}
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path d="M1.333 0h8v5.333H12V2.667h2.667V0h8v8H20v2.667h-2.667v2.666H20V16h2.667v8h-8v-2.667H12v-2.666H9.333V24h-8Z" />
+    </svg>
+  );
+}
+
+// Add new icons for check and x
+export function CheckIcon({
+  size = 16,
+  width,
+  height,
+  ...props
+}: IconSvgProps) {
+  return (
+    <svg
+      fill="none"
+      height={size || height}
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width={size || width}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M20 6L9 17l-5-5" />
+    </svg>
+  );
+}
+
+export function XIcon({ size = 16, width, height, ...props }: IconSvgProps) {
+  return (
+    <svg
+      fill="none"
+      height={size || height}
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width={size || width}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M18 6L6 18" />
+      <path d="M6 6l12 12" />
     </svg>
   );
 }
