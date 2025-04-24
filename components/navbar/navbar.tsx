63ecfbd7f91d7d7d4094f270c9c0c9058fabe646
saldanaj97 <saldanaj97@gmail.com>
2025-04-24 12:29:22 -0500

    refactor: rename PlatformLogin to PlatformLoginModal in Navbar component

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -12,7 +12,7 @@ import {
 } from "@heroui/navbar";
 import NextLink from "next/link";
 
-import PlatformLogin from "./platform-login";
+import PlatformLoginModal from "./PlatformLoginModal";
 
 import { Logo, SearchIcon } from "@/components/icons";
 import { siteConfig } from "@/config/site";
@@ -77,7 +77,7 @@ export const Navbar = () => {
 
       <NavbarContent className="hidden basis-1/4 sm:flex" justify="end">
         <ThemeSwitch />
-        <PlatformLogin />
+        <PlatformLoginModal />
       </NavbarContent>
 
       {/* Mobile Navbar */}
