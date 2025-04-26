7be647be02d73fc6fdc9ee371abad9546788df90
saldanaj97 <saldanaj97@gmail.com>
2025-04-26 16:34:46 -0500

    refactor: moved for clarity in file structure

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { Carousel } from "@/components/carousel/carousel";
 import TopStreams from "@/components/top-streams/TopStreams";
 
 export default function Home() {
   return (
     <main className="flex-1 px-4">
+      <Carousel />
       <h1 className="my-4 text-2xl font-bold md:text-3xl">Top Streams</h1>
       <TopStreams />
     </main>
