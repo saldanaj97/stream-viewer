ba83a67a9fad5d83ae9cdd21d9b872916c07945f
saldanaj97 <saldanaj97@gmail.com>
2025-04-26 16:40:43 -0500

    fix: update main layout for Home component to ensure proper alignment and spacing

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,7 @@ import TopStreams from "@/components/top-streams/TopStreams";
 
 export default function Home() {
   return (
-    <main className="flex-1 px-4">
+    <main className="flex-1 flex-col items-center justify-center p-4">
       <Carousel />
       <h1 className="my-4 text-2xl font-bold md:text-3xl">Top Streams</h1>
       <TopStreams />
