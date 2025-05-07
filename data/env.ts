a187144cbd30eb5cf8bbfe30688aa60bdbea749b
saldanaj97 <saldanaj97@gmail.com>
2025-05-06 21:15:32 -0500

    refactor: restore dynamic environment configuration for development and production

--- data/env.ts
+++ data/env.ts
@@ -1,4 +1,7 @@
 export const ENV = {
-  isDevelopment: false, //process.env.NODE_ENV === "development",
-  apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  isDevelopment: process.env.NODE_ENV === "development",
+  apiUrl:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8000"
+      : process.env.NEXT_PUBLIC_API_URL,
 };
