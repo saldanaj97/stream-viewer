f9cb4c6e5c8525e51b5d9e03494994a8615a9011
saldanaj97 <saldanaj97@gmail.com>
2025-05-16 14:12:35 -0500

    refactor: removed redundant markup

--- components/multistream-view/StreamItem.tsx
+++ components/multistream-view/StreamItem.tsx
@@ -89,24 +89,4 @@ export const StreamEmbed = ({ stream, index }: StreamItemProps) => {
       </div>
     </div>
   );
-
-  return (
-    <div className="relative h-full w-full overflow-hidden rounded-lg shadow-md">
-      <div
-        className={`${backgroundColor} absolute left-0 right-0 top-0 z-10 flex h-[26px] cursor-move items-center justify-between bg-opacity-50 px-2`}
-      >
-        <div className="stream-drag-handle h-full w-full cursor-move">
-          <span className="text-sm text-white">
-            {`Stream ${index + 1}`} {stream.channel} (
-            {stream.platform.slice(0, 1).toUpperCase() +
-              stream.platform.slice(1)}
-            )
-          </span>
-        </div>
-      </div>
-      <div className="h-full w-full overflow-hidden pt-[26px]">
-        {renderEmbed(stream)}
-      </div>
-    </div>
-  );
 };
